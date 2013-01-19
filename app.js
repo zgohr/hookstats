0b42196014cf55660664ce9fdfa5ea77ddd757fb
Zach Gohr <zachary.gohr@gmail.com>
2013-01-19 16:08:05 -0600

    Updated index to show pushes

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 'use strict';
 
 var FetcherCtrl = ['$scope', '$http', function($scope, $http){
-  var url = 'https://api.parse.com/1/classes/Checkout';
   var headers = {
-    'X-Parse-Application-Id': 'AJDr9PL0vCWndW87pkoIgCaV9v5YJxzuaFTfpi0M',
-    'X-Parse-REST-API-Key': 'r3mCXuXSTNQ7OHiX7u5jmLG5DPYWNQ7PTWNCXhNN'
+    'X-Parse-Application-Id': 'ElAH6C9vFmBLeO09O5qD0PgUfX0bA6v2zqiGap1L',
+    'X-Parse-REST-API-Key': 'HmBjwglSnpTmzzGK3BRqtu5zD1LQndTbH8LJde1f'
   };
+  var checkout_url = 'https://api.parse.com/1/classes/Checkout';
+  var push_url = 'https://api.parse.com/1/classes/Push';
   var params = {
     "order": "-createdAt"
   };
   $scope.commits = {};
-  $http.get(url, {headers: headers, params: params}).success(function(data, success){
+  $http.get(checkout_url, {headers: headers, params: params}).success(function(data, success){
     angular.forEach(data.results, function(val){
       if (!$scope.commits[val.user_email]) $scope.commits[val.user_email] = [];
       $scope.commits[val.user_email].push(val);
+    });
+  });
+  $scope.pushes = [];
+  $http.get(push_url, {headers: headers, params: params}).success(function(data, success){
+    angular.forEach(data.results, function(val){
+      $scope.pushes.push(val);
     });
   });
 }];
