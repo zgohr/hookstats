b440ba2dd9b584a04f5bc02317b27ffaa80aa5ce
Zachary Gohr <zachary.gohr@gmail.com>
2013-01-19 14:34:48 -0600

    Modified to use a development parse app.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 var FetcherCtrl = ['$scope', '$http', function($scope, $http){
   var url = 'https://api.parse.com/1/classes/Checkout';
   var headers = {
-    'X-Parse-Application-Id': 'AJDr9PL0vCWndW87pkoIgCaV9v5YJxzuaFTfpi0M',
-    'X-Parse-REST-API-Key': 'r3mCXuXSTNQ7OHiX7u5jmLG5DPYWNQ7PTWNCXhNN'
+    'X-Parse-Application-Id': 'ElAH6C9vFmBLeO09O5qD0PgUfX0bA6v2zqiGap1L',
+    'X-Parse-REST-API-Key': 'HmBjwglSnpTmzzGK3BRqtu5zD1LQndTbH8LJde1f'
   };
   var params = {
     "order": "-createdAt"
